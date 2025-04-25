07b4de6f6385116865cbdeed1b2f9f0bb119bf5a
luminance <ntv93575@gmail.com>
2025-04-26 01:25:15 +0900

    3주차 끝

--- project/src/app3.jsx
+++ project/src/app3.jsx
@@ -1,22 +1,53 @@
 //3주차 코드에요
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App3() {
+  let [userInfo, setUserInfo] = useState([]);
+  let [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(false);
+    axios //200ms postman mock API
+      .get("https://9f6e9fea-e8d5-4f6f-ac21-4cfa686caeeb.mock.pstmn.io/")
+      .then((result) => {
+        setUserInfo(result.data);
+        setIsLoading(true);
+      })
+      .catch(() => {
+        console.log("GET요청 실패");
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      <Asd>
-        <h4>hello</h4>
-      </Asd>
+      <div style={{ justifyContent: "left" }} className="nav-bar">
+        <h4>유저정보</h4>
+      </div>
+      {isLoading ? (
+        userInfo.map((user, i) => (
+          <UserCard key={i} userName={user.username}>
+            <h4>name: {user.name}</h4>
+            <h4>email: {user.email}</h4>
+          </UserCard>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
 
-function Asd({ children, title }) {
+function UserCard({ children, userName }) {
   return (
-    <div className="card">
-      <h2>{title}</h2>
-      {children}
+    <div className="cardCollector">
+      <div></div>
+      <div className="card">
+        <h2>nickname:{userName}</h2>
+        {children}
+      </div>
     </div>
   );
 }
